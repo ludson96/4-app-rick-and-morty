ee4ef68a9459a7310700d7ce91143c31487aa037
ludson96 <ludson_ps27@hotmail.com>
2026-03-04 18:02:11 -0300

    style: :lipstick: initiate styling cards in ListView

--- lib/pages/home/widgets/list_view_cards.widget.dart
+++ lib/pages/home/widgets/list_view_cards.widget.dart
@@ -1,3 +1,4 @@
+import 'package:app_rich_and_morty/colors.dart';
 import 'package:app_rich_and_morty/pages/home/store/home.store.dart';
 import 'package:flutter/material.dart';
 
@@ -17,15 +18,66 @@ class ListViewCards extends StatelessWidget {
           elevation: 10,
           child: SizedBox(
             height: 150,
-            child: Row(
-              children: [
-                Image.network(
-                  character.image,
-                  height: 100,
-                  width: 100,
-                  fit: BoxFit.cover,
-                ),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Image.network(
+                    character.image,
+                    height: 120,
+                    width: 120,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: .start,
+                      children: [
+                        Text(
+                          character.name,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: primaryColor,
+                            fontWeight: .w500,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Row(
+                          children: [
+                            Container(
+                              height: 10,
+                              width: 10,
+                              decoration: BoxDecoration(
+                                color: character.status == "Alive"
+                                    ? Colors.green
+                                    : Colors.red,
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              "${character.status} - ${character.species}",
+                              style: TextStyle(
+                                color: primaryColor,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          character.gender,
+                          style: TextStyle(color: primaryColor, fontSize: 16),
+                        ),
+                        Align(
+                          alignment: .centerRight,
+                          child: Text("${character.id}"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
